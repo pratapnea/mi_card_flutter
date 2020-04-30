28e53f5477ba885c999c2f44063a33370fe96160
Pratap Neaupane <sereversion@gmail.com>
2020-04-30 18:59:32 +0545

    package dependencies updated and main.dart file has been updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
